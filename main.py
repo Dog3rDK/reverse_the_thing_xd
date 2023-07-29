4613ceddde244daf07eef038a418a67ea2484b86
Aleksei Glazkov <dog3rdk@Alekseis-MacBook-Air.local>
2023-07-29 16:37:37 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def reverse_string(input_string):
+    reversed_string = input_string[::-1]
+    return reversed_string
+
+
+reverse_subject = input ('What do you want to reverse?')
+result1 = reverse_string(reverse_subject)
+print(result1)
+
